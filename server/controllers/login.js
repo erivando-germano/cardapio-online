27ffff9994585b89e6755c3edbb5b563728cac0a
erivando-germano <vandinhodtp@gmail.com>
2024-01-06 07:43:22 -0300

    método de login

---
+++ server/controllers/login.js
@@ -0,0 +1,14 @@
+
+
+const controllers = () => {
+    const login = async (req) => {
+        return { retorno: "OK" };
+    };
+
+    return Object.create({
+        login
+    })
+
+}
+
+module.exports = Object.assign({controllers})
